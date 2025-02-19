9d4755c9a78eab490e66f43de4f02f33161fb90f
adibbz2004 <rafiqoadib1@gmail.com>
2025-02-19 11:13:52 +0700

    Praktikum minggu ke 2 (Praktikum 1,2,3)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +23,57 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+   });
+
+Route::get('/world', function () {
+    return 'World';
+   });
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});  
+   
+Route::get('/about', function () {
+    return 'NIM: 2341720214 - Nama: Rafiqo Adib Destarachmad';
+}); 
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+    
+Route::get('/articles/{id}', function ($id) {
+        return "Halaman Artikel dengan ID ".$id;
+    });
+
+Route::get('/user/{name?}', function ($name=null) { return 'Nama saya '.$name;
+    });
+ 
+Route::get('/user/{name?}', function ($name='John') { return 'Nama saya '.$name;
+    });
+
+Route::get('/hello', [WelcomeController::class,'hello']);   
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+    
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
+
+Route::get('/greeting', function () {return view('hello', ['name' => 'Adib']);});
+
+Route::get('/greeting', function () {return view('blog.hello', ['name' => 'Adib']);});
+
+Route::get('/greeting', [WelcomeController::class,'greeting']);
